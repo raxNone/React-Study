c9151e567d2e92938cc9781de5a9c1b955ab6daa
raxNone <dryou04@kookmin.ac.kr>
2024-01-29 20:03:17 +0900

    React 기초 2강

--- react_study_app/src/App.js
+++ react_study_app/src/App.js
@@ -2,22 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let posts = '강남 고기 맛집';
+  let styles = {
+    color: 'blue', background: 'red',
+  }
+  function 함수(){
+    return 'hi'
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div style={styles}>React 학습 App</div>
+      </div>
+      <h4>{posts}</h4>
+      {함수()}
+      <img src = {logo}></img>
     </div>
   );
 }
