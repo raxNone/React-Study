7c4ea2c2029d97c31c34c3940e070742edf5c9d2
raxNone <dryou04@kookmin.ac.kr>
2024-01-29 20:25:03 +0900

    React 기초 3강

--- react_study_app/src/App.js
+++ react_study_app/src/App.js
@@ -1,22 +1,34 @@
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  let posts = '강남 고기 맛집';
-  let styles = {
-    color: 'blue', background: 'red',
-  }
-  function 함수(){
-    return 'hi'
-  }
+  
+
+  //state는 변수 대신 쓰는 데이터 저장공간
+  //state는 변경되면 HTML이 자동으로 재랜더링됨
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '여자 가디건 추천', '20대 신발 추천']);
+  //['남자코트추천', state 수정함수]
+  //[a,b] = [1,2] -> ES6 destructuring 문법
+  let [날짜, 날짜변경] = useState(['2월 17일 발행', '3월 1일 발행', '4월 9일 발행'])
+
   return (
     <div className="App">
       <div className="black-nav">
-        <div style={styles}>React 학습 App</div>
+        <div style={{color:'green'}}>React 학습 App</div>
       </div>
-      <h4>{posts}</h4>
-      {함수()}
-      <img src = {logo}></img>
+      <div className="list">
+        <h3>{글제목[0]}</h3>
+        <p>{날짜[0]}</p>
+        <hr/>
+        <h3>{글제목[1]}</h3>
+        <p>{날짜[1]}</p>
+        <hr/>
+        <h3>{글제목[2]}</h3>
+        <p>{날짜[2]}</p>
+        <hr/>
+      </div>
+      
     </div>
   );
 }
