73ce7d1798e29ce62e74ebbe1f4d6eeef6bdf684
raxNone <dryou04@kookmin.ac.kr>
2024-01-29 21:12:37 +0900

    React 기초 5강

--- react_study_app/src/App.js
+++ react_study_app/src/App.js
@@ -11,17 +11,24 @@ function App() {
   //['남자코트추천', state 수정함수]
   //[a,b] = [1,2] -> ES6 destructuring 문법
   let [날짜, 날짜변경] = useState(['2월 17일 발행', '3월 1일 발행', '4월 9일 발행'])
-
+  let [따봉, 따봉변경] = useState(0);
   return (
     <div className="App">
       <div className="black-nav">
         <div style={{color:'green'}}>React 학습 App</div>
       </div>
+      <button onClick={()=>{
+        var texts=[...글제목];
+        texts.sort();
+        글제목변경(texts);
+      }}>버튼</button>
       <div className="list">
-        <h3>{글제목[0]}</h3>
+        <h3>{글제목[0]} <span onClick={()=>{따봉변경(따봉+1)}}>G {따봉}</span> </h3>
         <p>{날짜[0]}</p>
         <hr/>
-        <h3>{글제목[1]}</h3>
+        <h3>{글제목[1]} <span onClick={()=>{ 
+          var newArray=[...글제목];//deep copy ...은 중괄호 제거된 채로 복사
+          newArray[1]='남자 가디건 추천';글제목변경(newArray)}}>s</span></h3>
         <p>{날짜[1]}</p>
         <hr/>
         <h3>{글제목[2]}</h3>
@@ -30,7 +37,7 @@ function App() {
       </div>
       
     </div>
-  );
+  );    
 }
 
 export default App;
