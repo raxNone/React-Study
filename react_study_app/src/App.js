84b6b100ff6a16881b9334007251c133e0877673
raxNone <dryou04@kookmin.ac.kr>
2024-02-01 16:03:00 +0900

    생활코딩 7강

--- react_study_app/src/App.js
+++ react_study_app/src/App.js
@@ -9,18 +9,41 @@ function App() {
     {id:2, title:'css', body:'css is...'},
     {id:3, title:'js', body:'js is...'},
   ]) 
+  const [mode,setMode] = useState('WELCOME');
+  const [id, setId] = useState(null);
+  let content=null;
+
+  if (mode==='WELCOME'){
+    content= <Article title='Welcome' body='Hello, WEB'/>
+  }else if (mode=='READ'){
+    topics.forEach(topic=>{
+      if (topic.id === id){
+    content= <Article title={topic.title} body={topic.body}/>}})
+  }
 
   return( 
     <div>
       <header>
-        <h2>TOPICS</h2>
+        <h2 onClick={()=>{setMode('WELCOME')}}>TOPIC</h2>
       </header>
-      <List topics={topics} onChangeMode={(body,id)=>{alert(id+' : '+body)}}/>
+      <List topics={topics} onChangeMode={id=>{console.log(id); setId(id); setMode('READ')}}/>
+
+      {content}
+
     </div>
   );
   
       
 }
+function Article(props){
+  return (
+    <div>
+      <p>{props.title}</p>
+      <p>{props.body}</p>
+    </div>
+  )
+}
+
 function List(props){
   
   let lis=[];
@@ -29,7 +52,7 @@ function List(props){
     lis.push(<li key={topic.id}><a id={topic.id} href={'/read/'+topic.id}
       onClick={function(event){
         event.preventDefault();
-        props.onChangeMode(topic.body,event.target.id);//event.target은 이벤트를 호출한 객체자체
+        props.onChangeMode(Number(event.target.id));//event.target은 이벤트를 호출한 객체자체
       }}
     >{topic.title}</a></li>)    
   });
