a12650468d348c70386ea284ab0ea73e5bc7fbf9
raxNone <dryou04@kookmin.ac.kr>
2024-02-01 14:53:36 +0900

    생활코딩 5강

--- react_study_app/src/App.js
+++ react_study_app/src/App.js
@@ -4,49 +4,38 @@ import './App.css';
 
 function App() {
   
+  let [topics,topicChange] = useState([
+    {id:1, title:'html', body:'html is...'},
+    {id:2, title:'css', body:'css is...'},
+    {id:3, title:'js', body:'js is...'},
+  ]) 
 
-  //state는 변수 대신 쓰는 데이터 저장공간
-  //state는 변경되면 HTML이 자동으로 재랜더링됨
-  let [글제목, 글제목변경] = useState(['남자 코트 추천', '여자 가디건 추천', '20대 신발 추천']);
-  //['남자코트추천', state 수정함수]
-  //[a,b] = [1,2] -> ES6 destructuring 문법
-  let [날짜, 날짜변경] = useState(['2월 17일 발행', '3월 1일 발행', '4월 9일 발행'])
-  let [따봉, 따봉변경] = useState(0);
-  return (
-    <div className="App">
-      <div className="black-nav">
-        <div style={{color:'green'}}>React 학습 App</div>
-      </div>
-      
-      <div className="list">
-        <h3>{글제목[0]} <span onClick={()=>{따봉변경(따봉+1)}}>G {따봉}</span> </h3>
-        <p>{날짜[0]}</p>
-        <hr/>
-        <h3>{글제목[1]} <span onClick={()=>{ 
-          var newArray=[...글제목];//deep copy ...은 중괄호 제거된 채로 복사
-          newArray[1]='남자 가디건 추천';글제목변경(newArray)}}>s</span></h3>
-        <p>{날짜[1]}</p>
-        <hr/>
-        <h3>{글제목[2]}</h3>
-        <p>{날짜[2]}</p>
-        <hr/>
-      </div>
-      
-      <Modal/>
-
+  return( 
+    <div>
+      <header>
+        <h2>TOPICS</h2>
+      </header>
+      <List topics={topics}/>
     </div>
-  );    
+  );
+  
+      
 }
-//필수 ! component는 첫글자 대문자
-//div쓰지 않고 묶고싶을 떄 <> </>
-function Modal(){
+function List(props){
+  
+  let lis=[];
+
+  props.topics.forEach(topic => {
+    lis.push(<li key={topic.id}><a href={'/read/'+topic.id}>{topic.title}</a></li>)    
+  });
+
   return (
-    <div className='modal'>
-      <h2>제목</h2>
-      <p>날짜</p>
-      <p>상세내용</p>
+    <div>
+      <ul>
+        {lis}
+      </ul>
     </div>
-  )
+  );
 }
 
 export default App;
